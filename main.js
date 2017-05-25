8b6e3d8b28896f84f526ca1338ca4cbf9ee9a162
spiderfencer <jinjoo@gmail.com>
2017-05-24 21:10:12 -0700

    next commit

---
+++ main.js
@@ -0,0 +1,22 @@
+window.onload = () => {
+ const plus = document.getElementsByClassName('b1');
+ for(let i=0;i<plus.length;i++){
+  plus[i].onclick = () => {
+   let value = document.getElementById('t'+(i+1)).value;
+   if(value.substring(0,4) !== "http" && value.substring(0,4) !== "file"){
+    value = "https://" + value;
+   }
+   document.getElementById('f'+(i+1)).src = value;
+  };
+ }
+ let i=0;
+ const areas = document.getElementsByTagName('textarea');
+ for(let i=0;i<areas.length;i++){
+  areas[i].onkeydown = k => {
+   if(k.keyCode === 13){
+    k.preventDefault();
+    document.getElementsByClassName('b1')[i].onclick();
+   }
+  }; 
+ }
+};
